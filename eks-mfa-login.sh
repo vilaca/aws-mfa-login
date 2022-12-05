d6928bfecd9e775c52cb93d0abc3c016d1c3ef59
João Vilaça <jvilaca@bigid.com>
2022-12-05 12:18:00 +0000

    edit .aws/credentials with credentials

--- eks-mfa-login.sh
+++ eks-mfa-login.sh
@@ -2,20 +2,24 @@
 set -e
 
 if [ ! $# -eq 4 ]; then
-  echo "Version: 0.0.1 -- See https://github.com/vilaca/eks-mfa-login for help."
+  echo "Version: 0.0.2 -- See https://github.com/vilaca/eks-mfa-login for help."
   FILENAME=$(basename "$BASH_SOURCE")
   echo "Usage: ./$FILENAME arn cluster region mfa-token"
   exit 1
 fi
 
 ARN=$1
-CLUSTER=$2
+PROFILE=$2
 REGION=$3
 MFA=$4
 
-SESSION_TOKEN=$(aws sts get-session-token --serial-number "$ARN" --token-code "$MFA")
-export AWS_ACCESS_KEY_ID=$(echo $SESSION_TOKEN | jq -r .Credentials.AccessKeyId)
-export AWS_SESSION_TOKEN=$(echo $SESSION_TOKEN | jq -r .Credentials.SessionToken)
-export AWS_SECRET_ACCESS_KEY=$(echo $SESSION_TOKEN | jq -r .Credentials.SecretAccessKey)
+CREDS=$(aws sts get-session-token --serial-number "$ARN" --token-code "$MFA")
+AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r .Credentials.AccessKeyId)
+AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r .Credentials.SecretAccessKey)
+AWS_SESSION_TOKEN=$(echo $CREDS | jq -r .Credentials.SessionToken)
 
-aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER"
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE
+aws configure set aws_session_token $AWS_SESSION_TOKEN --profile $PROFILE
+
+export AWS_PROFILE=$PROFILE
